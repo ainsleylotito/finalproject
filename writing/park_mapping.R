db14923f309eb84ce07355c112eadd27d5dc3d78
jacksondecook <215175249+jacksondecook@users.noreply.github.com>
2025-12-08 10:53:56 -0700

    More map work, added the boundaries and fixed the projections. Next, I will work on adding the points and colors.

--- writing/park_mapping.R
+++ writing/park_mapping.R
@@ -9,21 +9,31 @@ library(maps)
 
 st_layers("data/nps_boundary/")
 
-us_states <- map("state", plot = FALSE, fill = TRUE) %>% 
-  st_as_sf()
+us_states <- states(cb = TRUE) %>% 
+  filter(STUSPS %in% c(state.abb, "DC"))
+
+us_shifted <- tigris::shift_geometry(us_states)
+
+us_shifted <- st_transform(us_shifted, 4326)
 
 park_boundaries <- read_sf("data/nps_boundary/",
                            layer = "nps_boundary")
+
+parks_shifted <- tigris::shift_geometry(park_boundaries)
+
+parks_shifted <- st_transform(parks_shifted, 4326)
+
 
 head(park_boundaries)
 
 deaths <- read_csv("data/mortality_clean.csv")
 
 ggplot() + 
-  geom_sf(data = us_states, color = "white",
+  geom_sf(data = us_shifted, color = "white",
           fill = "darkgrey", alpha = 0.5) +
-  geom_sf(park_boundaries, color = "red",
-          alpha = 0.8)
+  geom_sf(data = parks_shifted, color = "red",
+          alpha = 0.8) +
+  xlim(c(-130, -65)) + ylim(c(20, 50))
 
 
 
